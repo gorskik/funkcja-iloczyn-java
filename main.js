4e7fa9dde48dc736472f7dc3d88aa69738f8807d
gorskik <kamilgr80@gmail.com>
2024-10-23 10:09:48 +0200

    funkcja-iloczyn

---
+++ main.js
@@ -0,0 +1,18 @@
+//ES5
+//function iloczyn(mun1, num2, num3) {
+  //  let result = num1 * num2 * num3;
+
+    //return result;
+//}
+
+//let wynikIloczynu = iloczyn(2,3,4);
+
+//console.log(wynikIloczynu);
+
+//ES6
+
+const iloczyn = (mun1, num2, num3) => num1 * num2 * num3;
+
+let wynikIloczynu = iloczyn(2,3,4);
+
+console.log(wynikIloczynu);
